38766f0a67ac64851e1fcc8dcb8a04b80b431dd4
AlexTrotsenko <olexii.trotsenko@gmail.com>
2018-08-15 17:37:40 +0200

    Made Runtime.callFunctionOn to be exported to Chrome DevTools - replace inheritance with delegation: Methods Dispatcher requires both args to be JsonRpcPeer and JSONObject

--- j2v8-debugger/src/main/java/com/alexii/j2v8debugger/Runtime.kt
+++ j2v8-debugger/src/main/java/com/alexii/j2v8debugger/Runtime.kt
@@ -3,6 +3,7 @@ package com.alexii.j2v8debugger
 import com.facebook.stetho.inspector.console.RuntimeReplFactory
 import com.facebook.stetho.inspector.jsonrpc.JsonRpcPeer
 import com.facebook.stetho.inspector.jsonrpc.JsonRpcResult
+import com.facebook.stetho.inspector.protocol.ChromeDevtoolsDomain
 import com.facebook.stetho.inspector.protocol.ChromeDevtoolsMethod
 import org.json.JSONObject
 import com.facebook.stetho.inspector.protocol.module.Runtime as FacebookRuntimeBase
@@ -13,12 +14,11 @@ import com.facebook.stetho.inspector.protocol.module.Runtime as FacebookRuntimeB
  * [initialize] must be called before actual debugging (adding breakpoints in Chrome DevTools).
  *  Otherwise setting breakpoint, etc. makes no effect.
  */
-class Runtime(
-        replFactory: RuntimeReplFactory?
-) : FacebookRuntimeBase(replFactory) {
+class Runtime(replFactory: RuntimeReplFactory?) : ChromeDevtoolsDomain {
+    val adaptee = FacebookRuntimeBase(replFactory)
 
     @ChromeDevtoolsMethod
-    override fun getProperties(peer: JsonRpcPeer?, params: JSONObject?): JsonRpcResult {
+    fun getProperties(peer: JsonRpcPeer?, params: JSONObject?): JsonRpcResult {
         /**
          * hack needed to return local variables: Runtime.getProperties called after Debugger.paused.
          * https://github.com/facebook/stetho/issues/611
@@ -27,24 +27,23 @@ class Runtime(
 
         params?.put("ownProperties", true)
 
-        val result = super.getProperties(peer, params)
+        val result = adaptee.getProperties(peer, params)
 
         return result
     }
 
     @ChromeDevtoolsMethod
-    override fun releaseObject(peer: JsonRpcPeer?, params: JSONObject?) = super.releaseObject(peer, params)
+    fun releaseObject(peer: JsonRpcPeer?, params: JSONObject?) = adaptee.releaseObject(peer, params)
 
     @ChromeDevtoolsMethod
-    override fun releaseObjectGroup(peer: JsonRpcPeer?, params: JSONObject?) = super.releaseObjectGroup(peer, params)
+    fun releaseObjectGroup(peer: JsonRpcPeer?, params: JSONObject?) = adaptee.releaseObjectGroup(peer, params)
 
     /**
      * Replaces [FacebookRuntimeBase.callFunctionOn] as override can't be used: CallFunctionOnResponse is private (return type)
      */
-//FIXME check why when we add following - Stetho is not working completely
-//    @ChromeDevtoolsMethod
-//    fun callFunctionOn(peer: JsonRpcPeer?, params: Any?): JsonRpcResult? = super.callFunctionOn(peer, params as JSONObject?)
+    @ChromeDevtoolsMethod
+    fun callFunctionOn(peer: JsonRpcPeer?, params: JSONObject?): JsonRpcResult? = adaptee.callFunctionOn(peer, params)
 
     @ChromeDevtoolsMethod
-    override fun evaluate(peer: JsonRpcPeer?, params: JSONObject?): JsonRpcResult = super.evaluate(peer, params)
+    fun evaluate(peer: JsonRpcPeer?, params: JSONObject?): JsonRpcResult = adaptee.evaluate(peer, params)
 }
